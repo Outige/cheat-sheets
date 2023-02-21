54cd9e1a86d641be41f97317ff0416fab96fe8dd
Tieg O'Sullivan <tiegosullivanpsnl@gmail.com>
2023-02-21 17:49:59 +0000

    Add python decorators cheats

---
+++ python/decorators.py
@@ -0,0 +1,42 @@
+from functools import wraps
+
+def decorator(function):
+    @wraps(function) # Required to get the correct __name__
+    def wrapper(*args, **kwargs):
+        print(1)
+        result = function(*args, **kwargs)
+        print(3)
+        return result
+    return wrapper
+
+def decorator_factory(factory_argument):
+    def decorator(function):
+        @wraps(function) # Required to get the correct __name__
+        def wrapper(*args, **kwargs):
+            print(f'Print: {factory_argument} from wrapper before decorated_function')
+            result = function(*args, **kwargs)
+            print(f'Print: {factory_argument} from wrapper after decorated_function')
+            return result
+        return wrapper
+    return decorator
+
+@decorator_factory('factory_argument')
+def decorated_by_decorator_factory(method_argument):
+    print(method_argument)
+    return 2
+
+@decorator
+def decorated_by_single_decorator(method_argument):
+    print(method_argument)
+    return 2
+
+if __name__ == '__main__':
+    print('decorated_by_decorator_factory:')
+    ressult = decorated_by_decorator_factory(method_argument='method_argument')
+    print(f'return value: {ressult}')
+    print(decorated_by_decorator_factory.__name__, '\n\n')
+
+    print('decorated_by_single_decorator:')
+    result = decorated_by_single_decorator(method_argument=2)
+    print(f'return value: {ressult}')
+    print(decorated_by_single_decorator.__name__)
